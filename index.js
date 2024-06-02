296e77e2ddd51f6dc82a2f6165cf44e01946f9db
Apurva Karpe <148335141+aprv-0@users.noreply.github.com>
2024-06-02 17:15:31 +0530

    fetch_user_update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.getElementById('fetch-posts').addEventListener('click', fetchPosts);
+document.getElementById('fetch-user-details').addEventListener('click', fetchUser);
 
 function fetchPosts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -17,4 +18,25 @@ function fetchPosts() {
             });
         })
         .catch(error => console.error('Error fetching posts:', error));
+}
+
+function fetchUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            const userDetailsContainer = document.getElementById('user-details-container');
+            userDetailsContainer.innerHTML = '';
+            data.forEach(user => {
+                const userElement = document.createElement('div');
+                userElement.classList.add('user');
+                userElement.innerHTML = `
+                    <h2>${user.name}</h2>
+                    <p>Email: ${user.email}</p>
+                    <p>Phone: ${user.phone}</p>
+                    <p>Website: ${user.website}</p>
+                `;
+                userDetailsContainer.appendChild(userElement);
+            });
+        })
+        .catch(error => console.error('Error fetching users:', error));
 }
